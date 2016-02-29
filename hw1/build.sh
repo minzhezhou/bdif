15106f51377964d6b5d44c84d67e44b80dd5ce2e
minzhezhou <minzhe.zhou@gmail.com>
2016-02-28 21:02:38 -0500

    update

--- hw1/build.sh
+++ hw1/build.sh
@@ -4,14 +4,16 @@ module load gcc/4.7
 module unload openmpi
 module load openmpi/1.8.4_gcc
 
+qsub gitcloneglog.pbs
+
 rm -rf glog_bld
 mkdir glog_bld
-cd glog_src
+cd glog
 ./configure --prefix=`pwd`/../glog_bld && make && make install
 cd ..
 
 mv glog_bld/include/* include
-mv glog_bld/lib/* lib
+mv glog_bld/lib  lib
 
 export LD_LIBRARY_PATH=`pwd`/lib:$LD_LIBRARY_PATH
 mpic++ -std=c++11 -I include -L lib -lglog hw1.cpp
