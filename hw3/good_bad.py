bad22ae4af93f420b0399c68528c97c84149bf9b
root <root@ip-172-31-43-55.ec2.internal>
2016-03-20 18:24:09 +0000

    up

--- hw3/good_bad.py
+++ hw3/good_bad.py
@@ -1,8 +1,7 @@
 import re
-import numpy as np
 import csv
 
-f = open('/Users/wyx/Documents/Baruch MFE/BDiF_yixiang_wang/assignment_C/LoughranMcDonald_MasterDictionary_2014.csv', 'rb')
+f = open('LoughranMcDonald_MasterDictionary_2014.csv', 'rb')
 reader = csv.reader(f)
 headers = reader.next()
 
@@ -13,43 +12,20 @@ for h in headers:
 for row in reader:
     for h, v in zip(headers, row):
         wordlist[h].append(v)
-
 f.close()
 
-print headers
-
+fp = open("words.txt","w")
 word_list={}
-
 for i in range(len(wordlist['Positive'])):
+    temp = 0
     if wordlist['Positive'][i]=='2009':
         temp=1
     elif wordlist['Negative'][i]=='2009':
         temp=-1
-    else:
-        temp=0
-    word_list[wordlist['Word'][i]]=temp
-    
-word_list['GOOD']
-    
+    if temp:
+      word_list[wordlist['Word'][i].lower()]=temp
+      fp.write(wordlist['Word'][i].lower() + "\t" + str(temp) + "\n")
 
-
-
-string_test='test:.[]da..gS***NOT////not..StuPid@@@@'
-
-stck_list={"JPM":1, "MS":1,"GS":1}
-
-
-
-
-def parse_tweet(src):
-    src_list=re.findall(r"[\w']+", src)     
-    res=[False,"NA",1]    
-    for i in src_list:
-        if i.upper() in stck_list.keys():
-            res[0]=True            
-            res[1]=i.upper()
-        if i.upper() in word_list.keys() and res[0]:
-            res[2]=res[2]*word_list[i.upper()]
-    return res
-    
-print parse_tweet(string_test)
+print len(word_list)
+fp.close()
+exit(0)
