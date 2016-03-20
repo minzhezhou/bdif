dc661566f84b077628e332ecbcb0130da3eca074
root <root@ip-172-31-43-55.ec2.internal>
2016-03-20 03:31:13 +0000

    add hw3

---
+++ hw3/good_bad.py
@@ -0,0 +1,55 @@
+import re
+import numpy as np
+import csv
+
+f = open('/Users/wyx/Documents/Baruch MFE/BDiF_yixiang_wang/assignment_C/LoughranMcDonald_MasterDictionary_2014.csv', 'rb')
+reader = csv.reader(f)
+headers = reader.next()
+
+wordlist = {}
+for h in headers:
+    wordlist[h] = []
+    
+for row in reader:
+    for h, v in zip(headers, row):
+        wordlist[h].append(v)
+
+f.close()
+
+print headers
+
+word_list={}
+
+for i in range(len(wordlist['Positive'])):
+    if wordlist['Positive'][i]=='2009':
+        temp=1
+    elif wordlist['Negative'][i]=='2009':
+        temp=-1
+    else:
+        temp=0
+    word_list[wordlist['Word'][i]]=temp
+    
+word_list['GOOD']
+    
+
+
+
+string_test='test:.[]da..gS***NOT////not..StuPid@@@@'
+
+stck_list={"JPM":1, "MS":1,"GS":1}
+
+
+
+
+def parse_tweet(src):
+    src_list=re.findall(r"[\w']+", src)     
+    res=[False,"NA",1]    
+    for i in src_list:
+        if i.upper() in stck_list.keys():
+            res[0]=True            
+            res[1]=i.upper()
+        if i.upper() in word_list.keys() and res[0]:
+            res[2]=res[2]*word_list[i.upper()]
+    return res
+    
+print parse_tweet(string_test)
---
+++ hw3/hw3.py
@@ -0,0 +1,47 @@
+import os
+import sys
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import SQLContext
+APP_NAME = "My Spark Application"
+
+def get_compid(strs):
+  compdic = {'boeing':"0", 'google':"1",  'amd':"2",  'dupont':"3", 'intel':"4",   'pfizer':"5",  'merck':"6",  'nike':"7",  'verizon':"8"}
+
+  res = [];
+  for s in strs:
+    if compdic.has_key(s.lower()):
+      res.append(compdic[s]);
+    return res;
+
+def str_date(x):
+  month={"Jan":"01","Feb":"02","Mar":"03"}
+  x1=x.split(" ")
+  y=x1[5]
+  m=month[x1[1]]
+  d=x1[2]
+  return "-".join([y,m,d])
+
+def mapp(x):
+  tmstp = str_date(x.created_at)
+  compid = get_compid(x.text)
+  res = []
+  for id in compid:
+    res.append(tmpstp + "\t" + id + "\t" + x.text)
+  if len(res):  
+    return res
+
+if __name__=="__main__":
+  conf = SparkConf().setAppName(APP_NAME)
+  conf = conf.setMaster("local[*]")
+  sc = SparkContext(conf=conf)
+  sqlContext = SQLContext(sc)
+  df = sqlContext.read.json("tws1/cleaned_2013_00.json")
+  df = df.filter(df["lang"].like('%en%'))
+  df=df.select(["created_at","text"])
+  rdd=df.rdd
+  rdd1=rdd.flatmap(mapp)
+  print rdd1.count()
+  rdd1.saveAsTextFile("tws3")
+  #df1=sc.parallelize(fr.collect()).saveAsTextFile("tweets/2013.txt")
+  #df1.write.save("tweets2/2013.txt",format="text")
+  #print df1
