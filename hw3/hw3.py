e0284aada7830f5298d3b5109d3b5775b6bfc27d
root <root@ip-172-31-43-55.ec2.internal>
2016-03-20 03:52:54 +0000

    up

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -8,9 +8,10 @@ def get_compid(strs):
   compdic = {'boeing':"0", 'google':"1",  'amd':"2",  'dupont':"3", 'intel':"4",   'pfizer':"5",  'merck':"6",  'nike':"7",  'verizon':"8"}
 
   res = [];
-  for s in strs:
+  scol = strs.split(" ")
+  for s in scol:
     if compdic.has_key(s.lower()):
-      res.append(compdic[s]);
+      res.append(compdic[s.lower()]);
     return res;
 
 def str_date(x):
@@ -26,22 +27,21 @@ def mapp(x):
   compid = get_compid(x.text)
   res = []
   for id in compid:
-    res.append(tmpstp + "\t" + id + "\t" + x.text)
-  if len(res):  
-    return res
+    res.append(tmstp + "\t" + id + "\t" + x.text)
+  return res
 
 if __name__=="__main__":
   conf = SparkConf().setAppName(APP_NAME)
   conf = conf.setMaster("local[*]")
   sc = SparkContext(conf=conf)
   sqlContext = SQLContext(sc)
-  df = sqlContext.read.json("tws1/cleaned_2013_00.json")
+  df = sqlContext.read.json("tws1")
   df = df.filter(df["lang"].like('%en%'))
   df=df.select(["created_at","text"])
   rdd=df.rdd
-  rdd1=rdd.flatmap(mapp)
+  rdd1=rdd.flatMap(mapp)
   print rdd1.count()
-  rdd1.saveAsTextFile("tws3")
+  rdd1.saveAsTextFile("tws4")
   #df1=sc.parallelize(fr.collect()).saveAsTextFile("tweets/2013.txt")
   #df1.write.save("tweets2/2013.txt",format="text")
   #print df1
